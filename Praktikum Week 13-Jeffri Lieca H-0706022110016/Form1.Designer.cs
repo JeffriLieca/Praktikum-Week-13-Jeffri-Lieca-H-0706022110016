2429fd585baa6d299cc310557e9936908b4b98dc
JeffriLieca <jhandoyo@student.ciputra.ac.id>
2022-05-23 13:12:07 +0700

    Add project files.

---
+++ Praktikum Week 13-Jeffri Lieca H-0706022110016/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace Praktikum_Week_13_Jeffri_Lieca_H_0706022110016
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFirst = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.dtpBirth = new System.Windows.Forms.DateTimePicker();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonPrev = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonLast = new System.Windows.Forms.Button();
+            this.numNumber = new System.Windows.Forms.NumericUpDown();
+            this.labelAvailable = new System.Windows.Forms.Label();
+            this.cBNatio = new System.Windows.Forms.ComboBox();
+            this.cBTeam = new System.Windows.Forms.ComboBox();
+            this.labelCap = new System.Windows.Forms.Label();
+            this.cbCap = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonFirst
+            // 
+            this.buttonFirst.Location = new System.Drawing.Point(115, 44);
+            this.buttonFirst.Name = "buttonFirst";
+            this.buttonFirst.Size = new System.Drawing.Size(68, 53);
+            this.buttonFirst.TabIndex = 0;
+            this.buttonFirst.Text = "<<";
+            this.buttonFirst.UseVisualStyleBackColor = true;
+            this.buttonFirst.Click += new System.EventHandler(this.buttonFirst_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Player ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Player Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(131, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "BirthDate";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(131, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nationality";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(131, 272);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Team";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(131, 306);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Team Number";
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(238, 117);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(100, 22);
+            this.tbID.TabIndex = 10;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(238, 155);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(100, 22);
+            this.tbName.TabIndex = 11;
+            // 
+            // dtpBirth
+            // 
+            this.dtpBirth.Location = new System.Drawing.Point(238, 195);
+            this.dtpBirth.Name = "dtpBirth";
+            this.dtpBirth.Size = new System.Drawing.Size(232, 22);
+            this.dtpBirth.TabIndex = 12;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(115, 357);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(105, 23);
+            this.buttonSave.TabIndex = 16;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(291, 357);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(105, 23);
+            this.buttonCancel.TabIndex = 17;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonPrev
+            // 
+            this.buttonPrev.Location = new System.Drawing.Point(238, 44);
+            this.buttonPrev.Name = "buttonPrev";
+            this.buttonPrev.Size = new System.Drawing.Size(68, 53);
+            this.buttonPrev.TabIndex = 18;
+            this.buttonPrev.Text = "<";
+            this.buttonPrev.UseVisualStyleBackColor = true;
+            this.buttonPrev.Click += new System.EventHandler(this.buttonPrev_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(345, 44);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(68, 53);
+            this.buttonNext.TabIndex = 19;
+            this.buttonNext.Text = ">";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonLast
+            // 
+            this.buttonLast.Location = new System.Drawing.Point(477, 44);
+            this.buttonLast.Name = "buttonLast";
+            this.buttonLast.Size = new System.Drawing.Size(68, 53);
+            this.buttonLast.TabIndex = 20;
+            this.buttonLast.Text = ">>";
+            this.buttonLast.UseVisualStyleBackColor = true;
+            this.buttonLast.Click += new System.EventHandler(this.buttonLast_Click);
+            // 
+            // numNumber
+            // 
+            this.numNumber.Location = new System.Drawing.Point(238, 306);
+            this.numNumber.Name = "numNumber";
+            this.numNumber.Size = new System.Drawing.Size(120, 22);
+            this.numNumber.TabIndex = 21;
+            this.numNumber.ValueChanged += new System.EventHandler(this.numNumber_ValueChanged);
+            // 
+            // labelAvailable
+            // 
+            this.labelAvailable.AutoSize = true;
+            this.labelAvailable.Location = new System.Drawing.Point(407, 311);
+            this.labelAvailable.Name = "labelAvailable";
+            this.labelAvailable.Size = new System.Drawing.Size(46, 17);
+            this.labelAvailable.TabIndex = 22;
+            this.labelAvailable.Text = "label7";
+            // 
+            // cBNatio
+            // 
+            this.cBNatio.FormattingEnabled = true;
+            this.cBNatio.Location = new System.Drawing.Point(238, 233);
+            this.cBNatio.Name = "cBNatio";
+            this.cBNatio.Size = new System.Drawing.Size(158, 24);
+            this.cBNatio.TabIndex = 23;
+            // 
+            // cBTeam
+            // 
+            this.cBTeam.FormattingEnabled = true;
+            this.cBTeam.Location = new System.Drawing.Point(238, 272);
+            this.cBTeam.Name = "cBTeam";
+            this.cBTeam.Size = new System.Drawing.Size(158, 24);
+            this.cBTeam.TabIndex = 24;
+            this.cBTeam.SelectedIndexChanged += new System.EventHandler(this.cBTeam_SelectedIndexChanged);
+            // 
+            // labelCap
+            // 
+            this.labelCap.AutoSize = true;
+            this.labelCap.Location = new System.Drawing.Point(421, 120);
+            this.labelCap.Name = "labelCap";
+            this.labelCap.Size = new System.Drawing.Size(46, 17);
+            this.labelCap.TabIndex = 25;
+            this.labelCap.Text = "label7";
+            // 
+            // cbCap
+            // 
+            this.cbCap.FormattingEnabled = true;
+            this.cbCap.Location = new System.Drawing.Point(488, 263);
+            this.cbCap.Name = "cbCap";
+            this.cbCap.Size = new System.Drawing.Size(121, 24);
+            this.cbCap.TabIndex = 26;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 450);
+            this.Controls.Add(this.cbCap);
+            this.Controls.Add(this.labelCap);
+            this.Controls.Add(this.cBTeam);
+            this.Controls.Add(this.cBNatio);
+            this.Controls.Add(this.labelAvailable);
+            this.Controls.Add(this.numNumber);
+            this.Controls.Add(this.buttonLast);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonPrev);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.dtpBirth);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonFirst);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFirst;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.DateTimePicker dtpBirth;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonPrev;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonLast;
+        private System.Windows.Forms.NumericUpDown numNumber;
+        private System.Windows.Forms.Label labelAvailable;
+        private System.Windows.Forms.ComboBox cBNatio;
+        private System.Windows.Forms.ComboBox cBTeam;
+        private System.Windows.Forms.Label labelCap;
+        private System.Windows.Forms.ComboBox cbCap;
+    }
+}
+
---
+++ Praktikum Week 13-Jeffri Lieca H-0706022110016/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Praktikum_Week_13_Jeffri_Lieca_H_0706022110016
+{
+    public partial class Form1 : Form
+    {
+        public static string sqlConnection = "server=localhost;uid=root;pwd=;database=premier_league";
+        public MySqlConnection sqlConnect = new MySqlConnection(sqlConnection);
+        public MySqlCommand sqlCommand;
+        public MySqlDataAdapter sqlAdapter;
+
+        
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static int counter = 0;
+        public static int lastData;
+        public string Nomor;
+        public string ID;
+        public static bool Avai;
+        public string Team;
+        public string Captain;
+        public bool Kaptenkah;
+        public string TimLama;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbCap.Visible = false;
+            labelCap.Visible = false;
+
+            sqlConnect.Open();
+            Perbarui();
+        }
+
+
+        public void Perbarui()
+        {
+            string sqlQuery = "select p.player_id as ID, p.player_name as Name, p.birthdate as Birth, p.nationality_id as NatioID , n.nation as Natio,p.team_id as TeamID, t.team_name as Team, p.team_number as Number  from player p,team t, nationality n where n.nationality_id=p.nationality_id and p.team_id=t.team_id;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            DataTable dtLoad = new DataTable();
+            sqlAdapter.Fill(dtLoad);
+            tbID.Text = dtLoad.Rows[counter][0].ToString();
+            ID = tbID.Text;
+            tbName.Text = dtLoad.Rows[counter][1].ToString();
+            dtpBirth.Text = dtLoad.Rows[counter][2].ToString();
+            lastData = dtLoad.Rows.Count-1;
+
+
+            sqlQuery = "select n.nationality_id as NatioID, n.nation as Natio from nationality n;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            DataTable dtNatio = new DataTable();
+            sqlAdapter.Fill(dtNatio);
+
+            cBNatio.DataSource = dtNatio;
+            cBNatio.DisplayMember = "Natio";
+            cBNatio.ValueMember = "NatioID";
+            cBNatio.Text = dtLoad.Rows[counter][4].ToString();
+
+            sqlQuery = "select t.team_id as TeamID, t.team_name as Team from team t;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            DataTable dtTeam = new DataTable();
+            sqlAdapter.Fill(dtTeam);
+
+            cBTeam.DataSource = dtTeam;
+            cBTeam.DisplayMember = "Team";
+            cBTeam.ValueMember = "TeamID";
+            cBTeam.Text = dtLoad.Rows[counter][6].ToString();
+            Team = dtLoad.Rows[counter][5].ToString();
+            TimLama = dtLoad.Rows[counter][5].ToString();
+
+            numNumber.Value = Convert.ToInt32(dtLoad.Rows[counter][7]);
+            Nomor = numNumber.Value.ToString();
+
+
+            
+                sqlQuery = "select t.team_id as IDTeam, t.team_name as NameTeam,p.player_name as Name, p.player_id as ID from team t, player p where p.player_id=t.captain_id and t.team_id='" + Team + "';";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                DataTable dtCaptain = new DataTable();
+                sqlAdapter.Fill(dtCaptain);
+                cbCap.DataSource = dtCaptain;
+                cbCap.DisplayMember = "Name";
+                cbCap.ValueMember = "ID";
+                
+            if (ID == dtCaptain.Rows[0][3].ToString())
+            {
+                labelCap.Text = "Captain";
+                Kaptenkah = true;
+            }
+            else
+            {
+                labelCap.Text = "Bukan Captain";
+                Kaptenkah = false;
+            }
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (counter == lastData)
+            {
+                MessageBox.Show("Sudah Data Terakhir");
+            }
+            else
+            {
+                counter++;
+                Perbarui();
+            }
+        }
+        private void buttonPrev_Click(object sender, EventArgs e)
+        {
+            if (counter == 0)
+            {
+                MessageBox.Show("Sudah Data Pertama");
+            }
+            else
+            {
+                counter--;
+                Perbarui();
+            }
+        }
+
+        private void buttonFirst_Click(object sender, EventArgs e)
+        {
+            counter = 0;
+            Perbarui();
+        }
+
+        private void buttonLast_Click(object sender, EventArgs e)
+        {
+            counter = lastData;
+            Perbarui();
+        }
+
+        private void numNumber_ValueChanged(object sender, EventArgs e)
+        {
+            string sqlQuery = "select p.player_name as Name,p.team_number as Number from team t,player p where p.team_id=t.team_id and t.team_id='"+cBTeam.SelectedValue.ToString()+"' having p.team_number='"+numNumber.Value+"';";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            DataTable dtNumber = new DataTable();
+            sqlAdapter.Fill(dtNumber);
+            if(dtNumber.Rows.Count == 0)
+            {
+                labelAvailable.Text = "Available";
+                Avai = true;
+            }
+            else
+            {
+                labelAvailable.Text = "Not Available";
+                Avai = false;
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (Avai == true)
+            {
+                if (Kaptenkah == true)
+                {
+                    if (cBTeam.SelectedValue == TimLama)
+                    {
+
+                    }
+                    else
+                    {
+                        string sqlUpdate = "update team set captain_id = (select player_id from player where team_id = '"+TimLama+"' order by birthdate limit 1)where team_id = '"+TimLama+"'; ";
+                        sqlCommand = new MySqlCommand(sqlUpdate, sqlConnect);
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                }
+                else
+                {
+
+                }
+                try
+                {
+                    string sqlUpdate = "update player set player_id='" + tbID.Text + "',team_number='" + numNumber.Value.ToString() + "', player_name='" + tbName.Text + "', nationality_id='" + cBNatio.SelectedValue.ToString() + "', birthdate='" + dtpBirth.Value.ToString("yyyyMMdd") + "',team_id='" + cBTeam.SelectedValue.ToString() + "' where player_id='" + ID + "'";
+                    sqlCommand = new MySqlCommand(sqlUpdate, sqlConnect);
+                    sqlCommand.ExecuteNonQuery();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Player ID sudah ada");
+                }
+                MessageBox.Show("Data sudah terupdate");
+
+                
+            }
+            else
+            {
+                MessageBox.Show("Team Number tidak tersedia");
+            }
+
+
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cBTeam_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string sqlQuery = "select p.player_name as Name,p.team_number as Number from team t,player p where p.team_id=t.team_id and t.team_id='" + cBTeam.SelectedValue.ToString() + "' having p.team_number='" + numNumber.Value + "';";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            DataTable dtNumber = new DataTable();
+            sqlAdapter.Fill(dtNumber);
+            if (dtNumber.Rows.Count == 0)
+            {
+                labelAvailable.Text = "Available";
+                Avai = true;
+            }
+            else
+            {
+                labelAvailable.Text = "Not Available";
+                Avai = false;
+            }
+        }
+    }
+}
